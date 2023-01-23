ecf80e1dc8476f8c19f7b741135fb5274a2ff7b5
amir <amirazizihosein@gmail.com>
2023-01-23 13:33:13 +0330

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
       lg: "976px",
       lx: "1440px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        cl_cyan: "hsl(179, 62%, 43%)",
+        cl_brightYellow: "hsl(71, 73%, 54%)",
+        // Neutral
+        cl_lightGray: "hsl(204, 43%, 93%)",
+        cl_grayishBlue: "hsl(218, 22%, 67%)",
+        cl_lightCyan: "hsl(179, 50%, 62%)",
+      },
+    },
   },
   plugins: [],
 };
